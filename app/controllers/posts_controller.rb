4df4fb64726c771ce207b2ccc75aaa27a14f25b5
ryoimahata <ryoimahata@gmail.com>
2021-06-01 13:56:13 +0900

    新規投稿機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,17 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.page(params[:page]).per(PER_PAGE).order(id: :desc)
+    @post = Post.new
   end
 
   def create
+    @post = Post.create!(post_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
