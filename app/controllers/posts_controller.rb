0631e9968aba23923a8dafeb631b0d248c977714
ryoimahata <ryoimahata@gmail.com>
2021-06-01 11:43:25 +0900

    ページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  PER_PAGE = 10
+
   def index
-    @posts = Post.order(id: :desc)
+    @posts = Post.page(params[:page]).per(PER_PAGE).order(id: :desc)
   end
 
   def create
